20ba360481df655c39d5dc30aa9479a2ccf9388c
Igor Feoktistov <igorf@netapp.com>
2019-08-08 14:04:36 -0700

    Minor changes in output formatting

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# WebServer example in Golang
+# WebServer example in Go
+
+This example shows how to build and deploy a containerized Go web server
+application using [Kubernetes](https://kubernetes.io).
+
+This directory contains:
+
+- `main.go` contains the HTTP server implementation. It answers to all HTTP
+requests with `Request Headers` in response.
+- `Dockerfile` is used to build the Docker image for the application.
+- `manifests` directory contains sample kubernetes manifests to deploy an application.
+
+This application is available as a Docker image:
+
+- `docker.io/iafeoktistov/go-webserver:1.0`
--- main.go
+++ main.go
@@ -2,25 +2,38 @@ package main
 
 import (
     "fmt"
+    "log"
     "net/http"
+    "os"
     "sort"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-
     var keys []string
+
+    host, _ := os.Hostname()
     for k := range r.Header {
         keys = append(keys, k)
     }
     sort.Strings(keys)
-    
-    fmt.Fprintln(w, "<b>Request Headers:</b></br>", r.URL.Path[1:])
+    log.Printf("Serving request: %s", r.URL.Path)
+    fmt.Fprintf(w, "<p>GoLang WebServer, version: 1.0.0</p>\n")
+    fmt.Fprintf(w, "<p><b>Hostname:</b> %s</p>\n", host)
+    fmt.Fprintf(w, "<p><b>Request Headers: </b></p>\n")
     for _, k := range keys {
-        fmt.Fprintln(w, k, ":", r.Header[k], "</br>", r.URL.Path[1:])
+        fmt.Fprintf(w, "<b>%s:</b> %s</br>\n", k, r.Header[k])
     }
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+    // PORT environment variable, default is 8080
+    port := "8080"
+    if fromEnv := os.Getenv("PORT"); fromEnv != "" {
+	port = fromEnv
+    }
+    server := http.NewServeMux()
+    server.HandleFunc("/", handler)
+    log.Printf("Server listening on port %s", port)
+    err := http.ListenAndServe(":" + port, server)
+    log.Fatal(err)
 }
